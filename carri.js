121e131ed07865d7d78730caf8a9517dad08fadf
Danilo Zurita <danizurita4@gmail.com>
2022-02-09 02:03:42 -0300

    Desafio

--- carri.js
+++ carri.js
@@ -8,6 +8,25 @@ let clickear = document.getElementById("compra1")
 
 function comprar () {
     let conficompra = document.getElementById("comprado");
-    conficompra.innerHTML = "<h2> Muchas Gracias Por Su compra</h2>"
+    let usuario1 = document.getElementById("nombre").value
+    let nombre2 = document.createElement("p")
+    conficompra.innerHTML = "<h2> Muchas Gracias Por Su compra</h2>" + usuario1;
     document.body.appendChild(comprado);
 }
+
+let clickear1 = document.getElementById("limpiar1")
+    clickear1.addEventListener("click", limpiar)
+
+    function limpiar () {
+        console.log("No compraron nada")
+
+
+    }
+
+    let mover = document.getElementById("letra")
+mover.onchange = () => {console.log("SeleccionaRRSS")};
+
+
+let input1 = document.getElementById("lista")
+input1.addEventListener('input' , () => {
+console.log(input1.value)})
