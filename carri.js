e59ba00efe0f4e741d13fd11c580f8cf638fba1b
Danilo Zurita <danizurita4@gmail.com>
2022-02-16 12:46:44 -0300

    2da prentrega

--- carri.js
+++ carri.js
@@ -1,18 +1,19 @@
 //Esto va en la pagina contacto//
-
-let usuario1 = {usuario:document.getElementById("nombre").value
-} 
-
 let clickear = document.getElementById("compra1")
       clickear.addEventListener("click", comprar)
 
+
+
 function comprar () {
     let conficompra = document.getElementById("comprado");
-    let usuario1 = document.getElementById("nombre").value
+    let usuario1 = document.getElementById("nombre").value;
     let nombre2 = document.createElement("p")
     conficompra.innerHTML = "<h2> Muchas Gracias Por Su compra</h2>" + usuario1;
     document.body.appendChild(comprado);
+
+console.log(usuario1);
 }
+let usuario1 = document.getElementById("nombre").value;
 
 let clickear1 = document.getElementById("limpiar1")
     clickear1.addEventListener("click", limpiar)
@@ -30,3 +31,10 @@ mover.onchange = () => {console.log("SeleccionaRRSS")};
 let input1 = document.getElementById("lista")
 input1.addEventListener('input' , () => {
 console.log(input1.value)})
+
+localStorage.setItem('usuario', JSON.stringify(usuario1));
+
+function recuperar() {
+let recuperarLS = JSON.parse(localStorage.getItem('usuario'))}
+
+recuperar();
